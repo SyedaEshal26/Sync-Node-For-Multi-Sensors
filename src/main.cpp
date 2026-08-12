9a1a7a9a236aa862c7a952198addbe01d3e2398a
Syeda Eshal Kashif <168112827+SyedaEshal26@users.noreply.github.com>
2026-08-13 01:01:47 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,21 @@
-#include <rclcpp/rclcpp.hpp>
 #include "sync_node_pkg/sync_node.hpp"
+
+#include <rclcpp/rclcpp.hpp>
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
 
-  auto node = std::make_shared<sync_node_pkg::SyncNode>();
+  auto node =
+    std::make_shared<sync_node_pkg::SyncNode>();
 
   rclcpp::executors::MultiThreadedExecutor executor;
+
   executor.add_node(node);
+
   executor.spin();
 
   rclcpp::shutdown();
+
   return 0;
 }
-

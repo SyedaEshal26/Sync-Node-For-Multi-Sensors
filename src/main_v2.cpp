f1be3991a2dab564ed5ef49706a4875742fb0b9c
Syeda Eshal Kashif <168112827+SyedaEshal26@users.noreply.github.com>
2026-08-24 09:23:17 +0500

    Simplify main function by using rclcpp::spin

--- src/main_v2.cpp
+++ src/main_v2.cpp
@@ -1,4 +1,3 @@
-#include <rclcpp/rclcpp.hpp>
 #include "sync_node_pkg/sync_node_v2.hpp"
 
 int main(int argc, char ** argv)
@@ -7,10 +6,7 @@ int main(int argc, char ** argv)
 
   auto node = std::make_shared<sync_node_pkg::SyncNodeV2>();
 
-  // MultiThreadedExecutor required for concurrent callbacks
-  rclcpp::executors::MultiThreadedExecutor executor;
-  executor.add_node(node);
-  executor.spin();
+  rclcpp::spin(node);
 
   rclcpp::shutdown();
   return 0;
